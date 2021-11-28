b50318960287afe3a7111f428f8f5cc60de50df3
Chahlie <cstewart2010@gmail.com>
2021-11-28 03:21:25 -0600

    it acutally works

---
+++ src/api/axiosHandler.js
@@ -0,0 +1,102 @@
+import axios from 'axios';
+import {NATURES, GENDERS, STATUSES} from './enums.json'
+
+/**
+ * Handles request for PTA
+ * @param {String} endpoint - endpoint to hit
+ * @param {String} method - method to hit endpoint with
+ * @param {String} activityToken the user's pta-activity-token
+ * @param {String} sessionAuth the user's pta-session-auth
+ * @returns An array with null reference in the first position and the response in the second position
+ */
+ export async function requestHandler(endpoint, method, activityToken, sessionAuth, data) {
+    let parsedResponse = null;
+    
+    let headers = {}
+    if (activityToken){
+        headers = {'pta-activity-token': activityToken, 'pta-session-auth': sessionAuth}
+    }
+
+    const config = {headers}
+    switch (method.toUpperCase()){
+        case "GET":
+            parsedResponse = await axios.get(endpoint, config).then(dataUpdater);
+            break;
+        case "POST":
+            parsedResponse = await axios.post(endpoint, data, config).then(dataUpdater);
+            break;
+        case "PUT":
+            parsedResponse = await axios.put(endpoint, data, config).then(dataUpdater);
+            break;
+        case "DELETE":
+            parsedResponse = await axios.delete(endpoint, config).then(dataUpdater);
+            break;
+        default:
+            throw `Argument was out of range: ${method}`
+    }
+
+    if (!parsedResponse){
+        throw `Failed to make ${method} request at ${endpoint}`
+    }
+
+    return parsedResponse;
+}
+
+/**
+ * Throws an exception if the argument is not a valid nature
+ * @param {String} nature 
+ */
+export function natureChecker(nature){
+    nullChecker(nature, 'nature');
+    if (!NATURES.includes(nature.toUpperCase())){
+        throw `Invalid nature ${nature}`
+    }
+}
+
+/**
+ * Throws an exception if the argument is not a valid gender
+ * @param {String} gender 
+ */
+export function genderChecker(gender){
+    nullChecker(gender, 'gender');
+    if (!GENDERS.includes(gender.toUpperCase())){
+        throw `Invalid gender ${gender}`
+    }
+}
+
+/**
+ * Throws an exception if the argument is not a valid status
+ * @param {String} status 
+ */
+export function statusChecker(status){
+    nullChecker(status, 'status');
+    if (!STATUSES.includes(status.toUpperCase())){
+        throw `Invalid status ${status}`
+    }
+}
+
+/**
+ * Throws an exception if the arugment is null or undefined
+ * @param argument the argument to check
+ * @param {String} argumentName the argument's name in the function
+ */
+export function nullChecker(argument, argumentName){
+    if (argument === null){
+        throw `null argument ${argumentName}`
+    }
+    if (argument === undefined){
+        throw `undefined argument ${argumentName}`
+    }
+}
+
+/**
+ * @param {import('axios').AxiosResponse} response 
+ * @returns A parsed version of the response
+ */
+const dataUpdater = response => {
+    return  {
+        data: response.data,
+        status: response.status,
+        headers: response.headers
+    }
+}
--- src/api/game.api.js
+++ src/api/game.api.js
@@ -1,78 +1,264 @@
-import axios from 'axios';
-
-const baseURL = "https://localhost:44344/api/v1/game"
-const client = axios.create({
-    baseURL
-});
+import { BASE_URL } from './api.config.json'
+import { METHODS } from './enums.json'
+import { requestHandler, nullChecker, natureChecker, statusChecker, genderChecker } from './axiosHandler';
+import { getPokemon } from './pokedex.api';
+const GAME_RESOURCE = `${BASE_URL}/api/v1/game`
 
 /**
- * 
- * @param {String} gameId 
- * @returns 
+ * @returns The last 20 game sessions generated
  */
-export async function findGame(gameId) {
-    try {
-        const {data} = await client.get(`/${gameId}`);
-        return [null, data];
-    }
-    catch (error) {
-        return [error]
-    }
+export async function findAllGames() {
+    return await requestHandler(GAME_RESOURCE, METHODS.GET);
 }
 
 /**
- * 
- * @param {String} gameId 
+ * @param {String} nickname the session nickname to limit the query by
+ * @returns All game sessions matching the nickname
+ */
+export async function findAllGamesByNickname(nickname) {
+    nullChecker(nickname, 'nickname');
+
+    return await requestHandler(`${GAME_RESOURCE}?nickname=${nickname}`, METHODS.GET);
+}
+
+/**
+ * Searches for a game session using its id
+ * @param {String} gameId the session id to search with
+ * @returns Returns the game session the matches the gameId
+ */
+export async function findGameById(gameId) {
+    nullChecker(gameId, 'gameId');
+
+    return await requestHandler(`${GAME_RESOURCE}/${gameId}`, METHODS.GET)
+}
+
+/**
+ * Searches for a trainer using their id
+ * @param {String} gameId the session id to search with
  * @param {String} trainerId 
  * @returns 
  */
 export async function findTrainerInGame(gameId, trainerId) {
-    return await client.get(`/${gameId}/${trainerId}`).catch(errorHandler);
+    nullChecker(gameId, 'gameId');
+    nullChecker(trainerId, 'trainerId');
+
+    return await requestHandler(`${GAME_RESOURCE}/${gameId}/find/${trainerId}`, METHODS.GET);
 }
 
-export async function createNewGame(){
-    try {
-        const {data} = await client.post("/new", null);
-        return [null, data];
+/**
+ * Creates a new game session and assigns it to a new game master
+ * @param {String} gmUsername the username for the game session's game master
+ * @param {String} gmPassword the game master's password
+ * @param {String} gameSessionPassword the game session's password
+ * @param {String} nickname the game nickname, if the game master wishes to set one
+ * @returns the Game data
+ */
+export async function createNewGame(gmUsername, gmPassword, gameSessionPassword, nickname = null){
+    nullChecker(gmUsername, 'gmUsername');
+    nullChecker(gmPassword, 'gmPassword');
+    nullChecker(gameSessionPassword, 'gameSessionPassword');
+
+    let endpoint = `${GAME_RESOURCE}/new?gmUsername=${gmUsername}&gmPassword=${gmPassword}&gameSessionPassword=${gameSessionPassword}`;
+    if (nickname){
+        endpoint = `${endpoint}&nickname=${nickname}`;
     }
-    catch (error) {
-        return [error]
+
+    return await requestHandler(endpoint, METHODS.POST, {});
+}
+
+/**
+ * Imports a game session for a json file
+ * @param {any} importFile the json file to send to the server
+ * @returns A 200 status code if successful
+ */
+export async function importGame(importFile){
+    nullChecker(importFile, 'importFile');
+    const data = new FormData();
+    data.append("json", importFile);
+    const config = {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    };
+
+    return await requestHandler(`${GAME_RESOURCE}/import`, METHODS.POST, config, data);
+}
+
+/**
+ * Generates a wild pokemon and assigns it to the game master
+ * @param {String} gameMasterId the game master's id
+ * @param {String} pokemon the pokemon's name in the pokedex
+ * @param {String} nature the pokemon's nature
+ * @param {String} gender the pokemon's gender
+ * @param {String} status the pokemon's status
+ * @param {String} activityToken the user's pta-activity-token
+ * @param {String} sessionAuth the user's pta-session-auth
+ * @param {String} nickname the pokemon's nickname
+ * @returns 
+ */
+export async function createWildPokemon(gameMasterId, pokemon, nature, gender, status, activityToken, sessionAuth, nickname){
+    nullChecker(gameMasterId, 'gameMasterId');
+    nullChecker(pokemon, 'pokemon');
+    nullChecker(activityToken, 'activityToken');
+    nullChecker(sessionAuth, 'sessionAuth');
+    natureChecker(nature);
+    genderChecker(gender);
+    statusChecker(status);
+    await getPokemon(pokemon)
+        .then(response => {
+            if (response.status != 200){
+                throw `Invalid pokemon ${pokemon}`
+            }
+        })
+
+    let endpoint = `${GAME_RESOURCE}/${gameMasterId}/wild?pokemon=${pokemon}&nature=${nature}&gender=${gender}&status=${status}`;    
+    if (nickname){
+        endpoint = `${endpoint}&nickname=${nickname}`
     }
+
+    return await requestHandler(endpoint, METHODS.POST, activityToken, sessionAuth)
 }
 
 /**
- * 
- * @param {String} gameId 
- * @param {boolean} isGM 
- * @param {Object} body 
- * @returns 
+ * Add a new player to an extant game session
+ * @param {String} gameId the session id to search with
+ * @param {String} trainerName the trainer's username
+ * @param {String} password the trainer's password
+ * @param {Number} attack the trainer's attack stat
+ * @param {Number} defense the trainer's defense stat
+ * @param {Number} specialAttack the trainer's special attack stat
+ * @param {Number} specialDefense the trainer's special defense stat
+ * @param {Number} speed the trainer's speed stat
+ * @returns the trainer data
  */
-export async function addPlayerToGame(gameId, isGM, body){
-    const endPoint = isGM
-        ? 'gm'
-        : 'new';
-    return await client.post(`/${gameId}/${endPoint}`, body).catch(errorHandler);
+export async function addPlayerToGame(gameId, trainerName, password, attack, defense, specialAttack, specialDefense, speed){
+    nullChecker(gameId, 'gameId');
+    nullChecker(trainerName, 'trainerName');
+    nullChecker(password, 'password');
+    nullChecker(attack, 'attack');
+    nullChecker(defense, 'defense');
+    nullChecker(specialAttack, 'specialAttack');
+    nullChecker(specialDefense, 'specialDefense');
+    nullChecker(speed, 'speed');
+
+    const endpoint = `${GAME_RESOURCE}/${gameId}/new?trainerName=${trainerName}&password=${password}&attack=${attack}&defense=${defense}&specialAttack=${specialAttack}&specialDefense=${specialDefense}&speed=${speed}`
+    return await requestHandler(endpoint, METHODS.PUT);
 }
 
 /**
- * 
- * @param {String} gameId 
- * @param {Object} body 
- * @returns 
+ * Sets a game sessions status to Online
+ * @param {String} gameId the session id to search with
+ * @param {String} gmUsername the username for the game session's game master
+ * @param {String} gmPassword the game master's password
+ * @param {String} gameSessionPassword the game session's password
+ * @returns A 200 status code if successful
  */
-export async function resetPassword(gameId, body){
-    return await client.put(`/${gameId}/reset`, body).catch(errorHandler);
+export async function startGame(gameId, gmUsername, gmPassword, gameSessionPassword){
+    nullChecker(gameId, 'gameId');
+    nullChecker(gmUsername, 'gmUsername');
+    nullChecker(gmPassword, 'gmPassword');
+    nullChecker(gameSessionPassword, 'gameSessionPassword');
+
+    const endpoint = `${GAME_RESOURCE}/${gameId}/start?gmUsername=${gmUsername}&gmPassword=${gmPassword}&gameSessionPassword=${gameSessionPassword}`;
+    return await requestHandler(endpoint, METHODS.PUT);
 }
 
 /**
- * 
- * @param {String} gameId 
- * @returns 
+ * Sets a game sessions status to Offline
+ * @param {String} gameId the session id to search with
+ * @param {String} gameMasterId the game master's id
+ * @param {String} activityToken the user's pta-activity-token
+ * @param {String} sessionAuth the user's pta-session-auth
+ * @returns A 200 status code if successful
  */
-export async function deleteGame(gameId){
-    return await client.delete(`/${gameId}`).catch(errorHandler);
+export async function endGame(gameId, gameMasterId, activityToken, sessionAuth){
+    nullChecker(gameId, 'gameId');
+    nullChecker(gameMasterId, 'gameMasterId');
+    nullChecker(activityToken, 'activityToken');
+    nullChecker(sessionAuth, 'sessionAuth');
+
+    return await requestHandler(`${GAME_RESOURCE}/${gameId}/end?gameMasterId=${gameMasterId}`, METHODS.PUT, activityToken, sessionAuth);
 }
 
-const errorHandler = (error) => {
-    return [error];
+/**
+ * Adds a set of npcs to a game session using the npcs' ids
+ * @param {String} gameId the session id to search with
+ * @param {String} npcList the npcs to add to the game
+ * @param {String} activityToken the user's pta-activity-token
+ * @param {String} sessionAuth the user's pta-session-auth
+ * @returns A 200 status code if successful
+ */
+export async function addNpcs(gameId, npcList, activityToken, sessionAuth){
+    nullChecker(gameId, 'gameId');
+    nullChecker(npcList, 'npcList');
+    nullChecker(activityToken, 'activityToken');
+    nullChecker(sessionAuth, 'sessionAuth');
+
+    return await requestHandler(`${GAME_RESOURCE}/${gameId}/addNpcs?npcList=${npcList}`, METHODS.PUT, activityToken, sessionAuth);
+}
+
+/**
+ * Removes a set of npcs to a game session using the npcs' ids
+ * @param {String} gameId the session id to search with
+ * @param {String} npcList the npcs to remove from the game
+ * @param {String} activityToken the user's pta-activity-token
+ * @param {String} sessionAuth the user's pta-session-auth
+ * @returns A 200 status code if successful
+ */
+export async function removeNpcs(gameId, npcList, activityToken, sessionAuth){
+    nullChecker(gameId, 'gameId');
+    nullChecker(npcList, 'npcList');
+    nullChecker(activityToken, 'activityToken');
+    nullChecker(sessionAuth, 'sessionAuth');
+
+    return await requestHandler(`${GAME_RESOURCE}/${gameId}/removeNpcs?npcList=${npcList}`, METHODS.PUT, activityToken, sessionAuth);
+}
+
+/**
+ * Resets a trainer password
+ * @param {String} trainerId the trainer id to search with
+ * @param {String} password the trainer's password
+ * @returns A 200 status code if successful
+ */
+export async function resetPassword(trainerId, password){
+    nullChecker(trainerId, 'trainerId');
+    nullChecker(password, 'password');
+
+    return await requestHandler(`${GAME_RESOURCE}/reset?trainerId=${trainerId}&password=${password}`, METHODS.PUT);
+}
+
+/**
+ * Export the game session from the server and deletes it
+ * @param {String} gameId the session id to search with
+ * @param {String} gameMasterId the game master's id
+ * @param {String} gameSessionPassword the game session's password
+ * @param {String} activityToken the user's pta-activity-token
+ * @param {String} sessionAuth the user's pta-session-auth
+ * @returns A json file matching the exported game
+ */
+export async function exportGame(gameId, gameMasterId, gameSessionPassword, activityToken, sessionAuth){
+    nullChecker(gameId, 'gameId');
+    nullChecker(gameMasterId, 'gameMasterId');
+    nullChecker(gameSessionPassword, 'gameSessionPassword');
+    nullChecker(activityToken, 'activityToken');
+    nullChecker(sessionAuth, 'sessionAuth');
+    
+    const endpoint = `${GAME_RESOURCE}/${gameId}/export?gameMasterId=${gameMasterId}&gameSessionPassword=${gameSessionPassword}`;
+    return await requestHandler(endpoint, METHODS.DELETE, activityToken, sessionAuth);
+}
+
+/**
+ * Delete the game session from the server
+ * @param {String} gameId the session id to search with
+ * @param {String} gameMasterId the game master's id
+ * @param {String} gameSessionPassword the game session's password
+ * @returns A 200 status code if successful
+ */
+export async function deleteGame(gameId, gameMasterId, gameSessionPassword){
+    nullChecker(gameId, 'gameId');
+    nullChecker(gameMasterId, 'gameMasterId');
+    nullChecker(gameSessionPassword, 'gameSessionPassword');
+    
+    const endpoint = `${GAME_RESOURCE}/${gameId}?gameMasterId=${gameMasterId}&gameSessionPassword=${gameSessionPassword}`;
+    return await requestHandler(endpoint, METHODS.DELETE);
 }
