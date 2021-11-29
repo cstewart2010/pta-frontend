362d77c2c59bf7e30b476896a924270f998755c7
Chahlie <cstewart2010@gmail.com>
2021-11-28 22:56:13 -0600

    created landing page

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,51 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <game-control />
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <landing-page v-if="needsToAuthenticate" />
+  <game-control v-else />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import GameControl from './components/GameControl.vue'
+import LandingPage from './components/LandingPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    GameControl
+    GameControl,
+    LandingPage
+  },
+  data() {
+      return {
+          gameId: null,
+          trainerId: null,
+          ptaActivityToken: null,
+          ptaSessionAuth: null,
+          needsToAuthenticate: true
+      }
+  },
+  mounted:function(){
+    const authString = window.localStorage.getItem("ptaAuthString");
+    if (!authString){
+      this.needsToAuthenticate = true
+      return;
+    }
+
+    try {
+      const auth = JSON.parse(authString);
+      this.ptaActivityToken = auth.ptaActivityToken
+      this.ptaSessionAuth = auth.ptaSessionAuth
+      this.trainerId = auth.trainerId
+      this.gameId = auth.gameId
+      this.needsToAuthenticate = auth.needsToAuthenticate
+    }
+    catch {
+      this.needsToAuthenticate = true
+    }
+    // authenticate trainer using api
+  },
+  methods: {
+    
   }
 }
 </script>
