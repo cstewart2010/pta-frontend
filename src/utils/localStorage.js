d6bb6198a9330887831cf52116f87425273b3ddd
Chahlie <cstewart2010@gmail.com>
2021-12-10 00:00:39 -0600

    added documentation to localstorage,js

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -5,6 +5,10 @@ const IS_GM = "isGM";
 const SESSION_AUTH = "ptaSessionAuth";
 const TRAINER_ID = "trainerId";
 
+/**
+ * Gets a logged in user's credentials
+ * @returns the trainerId, activityToken, and sessionAuth (in that order) as an array
+ */
 export function getUserCredentials(){
     return [
         getTrainerId(),
@@ -13,56 +17,106 @@ export function getUserCredentials(){
     ]
 }
 
+/**
+ * @returns the user's activity token
+ */
 export function getPTAActivityToken(){
     return localStorage.getItem(ACTIVITY_TOKEN);
 }
 
+/**
+ * @returns the user's gameId
+ */
 export function getGameId(){
     return localStorage.getItem(GAME_ID);
 }
 
+/**
+ * @returns true if a user is signed in
+ */
 export function getIsAuthenticate(){
     return getParsedItem(IS_AUTHENTICATE);
 }
 
+/**
+ * @returns true if the user is a gm
+ */
 export function getIsGM(){
     return getParsedItem(IS_GM);
 }
 
+/**
+ * @returns the user's sesstion auth
+ */
 export function getSessionAuth(){
     return localStorage.getItem(SESSION_AUTH);
 }
 
+/**
+ * @returns the user's trainer Id
+ */
 export function getTrainerId(){
     return localStorage.getItem(TRAINER_ID);
 }
 
+/**
+ * Adds the user's activity token to local storage
+ * @param {String} token 
+ */
 export function setPTAActivityToken(token){
     localStorage.setItem(ACTIVITY_TOKEN, token);
 }
 
+/**
+ * Adds the user's gameId to local storage
+ * @param {String} gameId 
+ */
 export function setGameId(gameId){
     localStorage.setItem(GAME_ID, gameId);
 }
 
+/**
+ * Adds the user's authentication status to local storage
+ * @param {Boolean} isAuthenticated 
+ */
 export function setIsAuthenticate(isAuthenticated){
     localStorage.setItem(IS_AUTHENTICATE, isAuthenticated);
 }
 
+/**
+ * Adds the user's player status to local storage
+ * @param {Boolean} isGM 
+ */
 export function setIsGM(isGM){
     localStorage.setItem(IS_GM, isGM);
 }
 
+/**
+ * Adds the user's session auth to local storage
+ * @param {String} sessionAuth 
+ */
 export function setSessionAuth(sessionAuth){
     localStorage.setItem(SESSION_AUTH, sessionAuth);
 }
 
+/**
+ * Adds the user's trainerId to local storage
+ * @param {String} trainerId 
+ */
 export function setTrainerId(trainerId){
     localStorage.setItem(TRAINER_ID, trainerId);
 }
 
+/**
+ * Removes all pta data from local storage
+ */
 export function removeFromStorage(){
     localStorage.clear();
 }
 
+/**
+ * Gets a json parsed item from local storage
+ * @param {String} key the item to parse
+ * @returns a jdon object
+ */
 const getParsedItem = key => JSON.parse(localStorage.getItem(key));
