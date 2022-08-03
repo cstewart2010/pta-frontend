c1196ce803ce0b6f9128682ce207e23219152ddd
Chahlie <cstewart2010@gmail.com>
2022-08-02 19:59:13 -0500

    Added functionality for npcs to the encounter

---
+++ src/components/encounter/NpcSheet.vue
@@ -0,0 +1,47 @@
+<template>
+    <div v-if="gameId">
+        <div class="row">
+            <div class="col-md-8">
+                <generic-data :npcId="npcId" />
+                <trainer-stats :trainerId="npcId" />
+                <trainer-skills :trainerId="npcId" />
+            </div>
+            <portrait :npcId="npcId" />
+        </div>
+        <hr>
+        <bottom :trainerId="npcId" />
+    </div>
+    <div class="d-flex justify-content-center" v-else>
+        <portrait :npcId="npcId" />
+    </div>
+</template>
+
+<script>
+import { getIsGM } from '../../utils/localStorage'
+import Bottom from './parts/Bottom.vue'
+import GenericData from './parts/NpcGenericData.vue'
+import Portrait from './parts/NpcPortrait.vue'
+import TrainerSkills from './parts/TrainerSkills.vue'
+import TrainerStats from './parts/TrainerStats.vue'
+
+export default {
+    name: "TrainerSheet",
+    props: {
+        npcId: {
+            default: ''
+        }
+    },
+    data(){
+        return {
+            gameId: getIsGM()
+        }
+    },
+    components: {
+        Bottom,
+        GenericData,
+        Portrait,
+        TrainerSkills,
+        TrainerStats
+    }
+}
+</script>
--- src/components/encounter/TrainerSheet.vue
+++ src/components/encounter/TrainerSheet.vue
@@ -1,14 +1,24 @@
 <template>
-    <div class="row">
-        <div class="col-md-8">
-            <generic-data :trainerId="trainerId" />
-            <trainer-stats :trainerId="trainerId" />
-            <trainer-skills :trainerId="trainerId" />
+    <div v-if="isGM || trainerId == currentTrainerId">
+        <div class="row">
+            <div class="col-md-8">
+                <generic-data :trainerId="trainerId" />
+                <trainer-stats :trainerId="trainerId" />
+                <trainer-skills :trainerId="trainerId" />
+            </div>
+            <portrait :trainerId="trainerId" />
         </div>
-        <portrait :trainerId="trainerId" />
+        <hr>
+        <bottom :trainerId="trainerId" />
     </div>
-    <hr>
-    <bottom :trainerId="trainerId" />
+    <div v-else>
+        <div class="row">
+            <generic-data :trainerId="trainerId" />
+        </div>
+        <div class="d-flex justify-content-center">
+            <portrait :trainerId="trainerId" />
+        </div>
+    </div>
 </template>
 
 <script>
@@ -17,12 +27,19 @@ import TrainerSkills from './parts/TrainerSkills.vue'
 import Portrait from './parts/Portrait.vue'
 import TrainerStats from './parts/TrainerStats.vue'
 import Bottom from './parts/Bottom.vue'
+import { getIsGM, getTrainerId } from '../../utils/localStorage'
 
 export default {
     name: "TrainerSheet",
     props: {
         trainerId: {
             default: ''
+        }
+    },
+    data(){
+        return {
+            isGM: getIsGM(),
+            currentTrainerId: getTrainerId()
         }
     },
     components: {
