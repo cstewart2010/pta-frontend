c84bcf70effe8cfd1fd25dadcb6bf776a98f2a00
Chahlie <cstewart2010@gmail.com>
2022-10-07 14:18:01 -0500

    Moved Origin to beneath portrait

--- src/components/trainer/parts/Left.vue
+++ src/components/trainer/parts/Left.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-7">
             <generic-data />
             <trainer-stats />
             <trainer-skills />
